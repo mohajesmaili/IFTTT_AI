bb844c1f31e32bd6da80a1a2226cf2bd89d04e7c
mo.hajesmaili <mo.hajesmaili@gmail.com>
2020-08-15 16:11:52 +0430

    adding files to host

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Project/Code_Igniter/IFTTT_Al/';
+$config['base_url'] = 'https://mohajesmaili.ir/';
 /*
 |--------------------------------------------------------------------------
 | Index File
